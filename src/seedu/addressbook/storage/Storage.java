95e68f93e7ec58c7ca33cd6a4386a2e6155aaa6a
Wei Kang <weikangchia@gmail.com>
2016-09-29 20:17:18 +0800

    Invert dependency from Logic to Storage Apply Dependency Inversion Principle

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,23 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+public abstract class Storage {
+    
+    /**
+     * Saves all data to this storage file.
+     *
+     * @throws StorageOperationException if there were errors converting and/or storing data to file.
+     */
+    public abstract void save(AddressBook addressBook) throws StorageOperationException;
+    
+    /**
+     * Loads data from this storage file.
+     *
+     * @throws StorageOperationException if there were errors reading and/or converting data from file.
+     */
+    public abstract AddressBook load() throws StorageOperationException;
+    
+    public abstract String getPath();
+}
